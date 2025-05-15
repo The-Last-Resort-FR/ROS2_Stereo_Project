b88b84613fadc87e48503d9f2bec1fa6e6ad2b8a
TheLastResort <camille.colimard@gmail.com>
2025-05-15 14:21:49 +0200

    fixing run script

--- run.sh
+++ run.sh
@@ -16,9 +16,9 @@ cleanup() {
 
 trap cleanup SIGINT
 
-ros2 launch stm_comm stm_comm launch.py > stm_comm.log 2>&1 &
+ros2 launch stm_comm launch.py > stm_comm.log 2>&1 &
 ros2 launch camera_manager launch.py > camera_manager.log 2>&1 &
-ros2 launch image_processor image_processor launch.py > image_processor.log 2>&1 &
+ros2 launch image_processor launch.py > image_processor.log 2>&1 &
 
 echo "All process launched"
 
